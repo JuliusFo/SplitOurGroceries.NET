6461fc95185c86ee6567f8cf9ba58097eec36bfb
Julius Forner <j.forner@ccc-software.de>
2024-05-12 13:47:42 +0200

    Add project files.

---
+++ SplitOurGroceries/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace SplitOurGroceries
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ SplitOurGroceries/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SplitOurGroceries
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
