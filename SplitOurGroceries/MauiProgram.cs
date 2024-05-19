a8269c74d5ad7cc0ba8f8968176ae73543879bd3
Julius Forner <j.forner@ccc-software.de>
2024-05-19 21:25:34 +0200

    Fixed missing package use.

--- SplitOurGroceries/MauiProgram.cs
+++ SplitOurGroceries/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using DevExpress.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace SplitOurGroceries
 {
@@ -9,6 +10,7 @@ namespace SplitOurGroceries
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseDevExpress()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
