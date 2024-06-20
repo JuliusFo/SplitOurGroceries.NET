fc68c11564b6923d16dfcc5629fe7b2a5fb4232d
Julius Forner <j.forner@ccc-software.de>
2024-06-20 19:48:16 +0200

    Added commands for item addition and data manipulation.

--- SplitOurGroceries.Content/ItemAddition/ViewModels/ItemAdditionViewModel.cs
+++ SplitOurGroceries.Content/ItemAddition/ViewModels/ItemAdditionViewModel.cs
@@ -21,11 +21,35 @@ public class ItemAdditionViewModel
     public ItemAdditionViewModel()
     {
         Model = new SplitItemModel(string.Empty, 0f);
+
+        #region Commands
+
+        DecreaseQuantityCommand = new Command(DecreaseQuantity);
+        IncreaseQuantityCommand = new Command(IncreaseQuantity);
+        ScanBarcodeCommand = new Command(ScanBarcode);
+        CancelCommand = new Command(Cancel);
+        ConfirmCommand = new Command(Confirm);
+
+        #endregion
     }
 
     #endregion
 
     #region Properties
+
+    #region Commands
+
+    public Command DecreaseQuantityCommand { get; }
+
+    public Command IncreaseQuantityCommand { get; }
+    
+    public Command ScanBarcodeCommand { get; }
+
+    public Command CancelCommand { get; }
+
+    public Command ConfirmCommand { get; }
+
+    #endregion
 
     internal ItemAdditionResult? Data { get; set; }
 
@@ -35,7 +59,38 @@ public class ItemAdditionViewModel
 
     #region Methods
 
+    #region Data manipulation
 
+    private void DecreaseQuantity()
+    {
+
+    }
+
+    private void IncreaseQuantity()
+    {
+
+    }
+
+    private void ScanBarcode()
+    {
+
+    }
+
+    #endregion
+
+    #region Bottom buttons
+
+    private void Cancel()
+    {
+
+    }
+
+    private void Confirm()
+    {
+
+    }
+
+    #endregion
 
     #endregion
 }
